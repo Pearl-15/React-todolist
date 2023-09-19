2ef437251b52da1477b1e87de87d64c4467b2ec5
Pearl-15 <naruto.pk15@gmail.com>
2023-09-19 12:10:50 +0800

    improve: improve footer style instead of setting App.css, move to FooterSection component

--- src/components/FooterSection.js
+++ src/components/FooterSection.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Layout,Typography } from "antd";
+import styled from 'styled-components';
 const { Footer } = Layout;
 const { Text } = Typography;
+
+const StyledFooter = styled.footer`
+  background-color: grey;
+  color: white;
+  text-align: center;
+  padding: 10px;
+  margin-top: 20px;
+
+  .ant-typography {
+    color: white; 
+  }
+`;
 
 class FooterSection extends React.Component{
 
     render(){
         const currentYear = new Date().getFullYear();
         return(
-            <Footer className='footerSection'>
+            <StyledFooter className='footerSection'>
                 <Text>Copyright &copy; {currentYear}</Text>
-            </Footer>
+            </StyledFooter>
         )
        
     }
--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Form, Input } from 'antd';
-import styled from 'styled-components';
+
 
 const { TextArea } = Input;
 
